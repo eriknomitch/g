2df2508990c30e2b347d64da5a5be2a8d757b79a
Erik Nomitch <enomitch@gmail.com>
2017-02-13 11:22:33 -0600

    Support .directory-metadata

--- g.zsh
+++ g.zsh
@@ -2,6 +2,39 @@
 # G ==============================================
 # ================================================
 
+# ------------------------------------------------
+# COLORS -----------------------------------------
+# ------------------------------------------------
+# 30 - black
+# 31 - red
+# 32 - green
+# 33 - yellow
+# 34 - blue
+# 35 - magenta
+# 36 - cyan
+# 37 - white
+# 
+# 0 - normal
+# 1 - bold
+# 2 - normal again
+# 3 - background color
+# 4 - underline the text
+# 5 - blinking
+
+
+# http://stackoverflow.com/questions/16843382/colored-shell-script-output-library
+RCol='\e[0m'    # Text Reset
+
+# Regular           Bold                Underline           High Intensity      BoldHigh Intens     Background          High Intensity Backgrounds
+Bla='\e[0;30m';     BBla='\e[1;30m';    UBla='\e[4;30m';    IBla='\e[0;90m';    BIBla='\e[1;90m';   On_Bla='\e[40m';    On_IBla='\e[0;100m';
+Red='\e[0;31m';     BRed='\e[1;31m';    URed='\e[4;31m';    IRed='\e[0;91m';    BIRed='\e[1;91m';   On_Red='\e[41m';    On_IRed='\e[0;101m';
+Gre='\e[0;32m';     BGre='\e[1;32m';    UGre='\e[4;32m';    IGre='\e[0;92m';    BIGre='\e[1;92m';   On_Gre='\e[42m';    On_IGre='\e[0;102m';
+Yel='\e[0;33m';     BYel='\e[1;33m';    UYel='\e[4;33m';    IYel='\e[0;93m';    BIYel='\e[1;93m';   On_Yel='\e[43m';    On_IYel='\e[0;103m';
+Blu='\e[0;34m';     BBlu='\e[1;34m';    UBlu='\e[4;34m';    IBlu='\e[0;94m';    BIBlu='\e[1;94m';   On_Blu='\e[44m';    On_IBlu='\e[0;104m';
+Pur='\e[0;35m';     BPur='\e[1;35m';    UPur='\e[4;35m';    IPur='\e[0;95m';    BIPur='\e[1;95m';   On_Pur='\e[45m';    On_IPur='\e[0;105m';
+Cya='\e[0;36m';     BCya='\e[1;36m';    UCya='\e[4;36m';    ICya='\e[0;96m';    BICya='\e[1;96m';   On_Cya='\e[46m';    On_ICya='\e[0;106m';
+Whi='\e[0;37m';     BWhi='\e[1;37m';    UWhi='\e[4;37m';    IWhi='\e[0;97m';    BIWhi='\e[1;97m';   On_Whi='\e[47m';    On_IWhi='\e[0;107m';
+
 # TODO: g cmp should check to see if the last commit had the same message as the
 # one you're trying to create and it should prompt.
 
@@ -71,6 +104,13 @@ _g_chpwd()
   
   if ( `pwd-is-git-repo --root` ) ; then
     echo -e "\033[30;1mpwd:\033[0m \033[33;2m`pwd-tilde`\033[0m"
+  fi
+
+  if [[ -f .directory-metadata ]] ; then
+    source .directory-metadata
+    echo
+    echo "- ${BPur}$_directory_title${RCol} -"
+    echo
   fi
 }
 
